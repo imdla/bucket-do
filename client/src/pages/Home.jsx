aff18e2b20029166ef97f5b14db440e9bcff121e
imdla <imdla918@gmail.com>
2025-02-22 16:39:36 +0900

    [BD-29] style: Home 페이지 스타일 지정

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
 import BucketList from '../components/BucketList';
 import Footer from '../components/Footer';
+import styles from '../styles/Home.module.css';
 
 function Home() {
+  const [isActive, setIsActive] = useState();
+
   return (
     <div>
       <Header></Header>
-      <div>
-        <BucketList></BucketList>
-      </div>
+
+      <section>
+        <div className={styles.container}>
+          <>
+            <ul className={styles.filter}>
+              <li>
+                <button>모두</button>
+              </li>
+              <li>
+                <button>진행중</button>
+              </li>
+              <li>
+                <button>완료</button>
+              </li>
+            </ul>
+          </>
+
+          <div className={styles.bucketList}>
+            <BucketList></BucketList>
+          </div>
+        </div>
+      </section>
+
       <Footer></Footer>
     </div>
   );
