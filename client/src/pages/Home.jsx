2d87927dddf09c6ee5e14c1bfada8f606e860851
imdla <imdla918@gmail.com>
2025-02-22 16:52:19 +0900

    [BD-29] feat: filter 버튼 클릭 시 handleClick 이벤트 추가

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import BucketList from '../components/BucketList';
 import Footer from '../components/Footer';
 import styles from '../styles/Home.module.css';
 
 function Home() {
-  const [isActive, setIsActive] = useState();
+  const [currentFilter, setCurrentFilter] = useState('모두');
+
+  function handleClick(e) {
+    const filterValue = e.target.innerHTML;
+    setCurrentFilter(filterValue);
+  }
 
   return (
     <div>
@@ -16,13 +21,13 @@ function Home() {
           <>
             <ul className={styles.filter}>
               <li>
-                <button>모두</button>
+                <button onClick={handleClick}>모두</button>
               </li>
               <li>
-                <button>진행중</button>
+                <button onClick={handleClick}>진행중</button>
               </li>
               <li>
-                <button>완료</button>
+                <button onClick={handleClick}>완료</button>
               </li>
             </ul>
           </>
