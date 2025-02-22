b45d62f42170096612a89f40353e427f66fd63ac
sungjoon92 <wsjtjdwns92@naver.com>
2025-02-22 17:33:23 +0900

    [BD-26] feat: 회원가입 페이지 레이아웃, 스타일, 유효성 검사
    1. 회원가입 페이지 레이아웃 작성
    2. 회원가입 페이지 스타일 작성
    3. 회원가입 페이지 아이디, 이메일, 연락처, 비밀번호 유효성 검사 완료

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,28 +1,80 @@
 import React, { useState } from 'react';
 import styles from '../styles/Signup.module.css';
 function Signup() {
+  const [id, setId] = useState('');
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [tel, setTel] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState('');
+  const [passwordConfirmed, setPasswordConfirmed] = useState(false);
+
+  // id 유효성 검사
+  const idCheck = () => {
+    // if(emailValue === DB에 있는 email 데이터){
+    //   alert("이미 존재하는 이메일입니다")
+    // }else{
+    //   alert("이메일 중복확인 완료!")
+    // }
+  };
+
+  // email 유효성 검사
+  const emailCheck = () => {
+    // if(emailValue === DB에 있는 email 데이터){
+    //   alert("이미 존재하는 이메일입니다")
+    // }else{
+    //   alert("이메일 중복확인 완료!")
+    // }
+  };
+
+  // 비밀번호 확인 메시지 설정
+  let passwordMessage = '';
+  if (passwordCheck) {
+    passwordMessage =
+      password === passwordCheck ? '비밀번호가 일치합니다!' : '비밀번호가 일치하지 않습니다!';
+  }
+
+  const confirm = (e) => {
+    e.priventDefault;
+    if (password != passwordCheck) {
+      alert('비밀번호가 일치하지 않습니다');
+      return;
+    }
+  };
   return (
     <div className={styles.signupBackGround}>
       <div className={styles.signupContainer}>
         <form className={styles.signupForm}>
-          <div>
+          <div className={styles.DuplicateCheckBox}>
+            <input
+              className={styles.signupInput}
+              type="text"
+              name="id"
+              value={id}
+              placeholder="아이디"
+              required
+              onChange={(e) => setId(e.target.value)}
+            />
+            <button className={styles.emailCheckButton} onClick={emailCheck}>
+              중복 확인
+            </button>
+          </div>
+          <div className={styles.DuplicateCheckBox}>
             <input
               className={styles.signupInput}
               type="email"
               name="email"
+              value={email}
               placeholder="이메일"
               required
+              onChange={(e) => setEmail(e.target.value)}
             />
             {/* DB 에 존재하는지 정보 받기 */}
-            <button className={styles.emailCheckButton}>중복 확인</button>
+            <button className={styles.emailCheckButton} onClick={emailCheck}>
+              중복 확인
+            </button>
           </div>
-          <input
-            className={styles.signupInput}
-            type="text"
-            name="username"
-            placeholder="닉네임"
-            required
-          />
+
           <input
             className={styles.signupInput}
             type="tel"
@@ -36,6 +88,8 @@ function Signup() {
             type="text"
             name="password"
             placeholder="비밀번호"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             required
           />
           <input
@@ -43,16 +97,21 @@ function Signup() {
             type="text"
             name="passwordCheck"
             placeholder="비밀번호 확인"
+            value={passwordCheck}
+            onChange={(e) => setPasswordCheck(e.target.value)}
             required
           />
           <input
             className={styles.passwordConfirmed}
             type="text"
             name="passwordConfirmed"
-            placeholder="비밀번호가 확인되었습니다"
+            placeholder={passwordMessage} // 상태에 따라 메시지 변경
+            value={passwordMessage}
             disabled
           />
-          <button className={styles.signupButton}>회원가입</button>
+          <button className={styles.signupButton} onClick={(e) => confirm(e)} type="submit">
+            회원가입
+          </button>
         </form>
       </div>
     </div>
