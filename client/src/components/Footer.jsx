3136c952aae5351b31a9700bfb757e45affd3bb7
imdla <imdla918@gmail.com>
2025-02-22 15:26:37 +0900

    [BD-33] feat: Footer 구조 작성

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <p>© 2025. BucketDo. All Rights Reserved.</p>
+    </footer>
+  );
 }
 
 export default Footer;
