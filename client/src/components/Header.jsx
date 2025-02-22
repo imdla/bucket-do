cdb9320833fb0fe42d2d3d9679d7ee4d57b260b7
imdla <imdla918@gmail.com>
2025-02-22 14:00:39 +0900

    [BD-32] feat: Header 컴포넌트 구조 작성성
    - Header (logo, logout button)

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div>
+        <>
+          <h1>
+            <img src="../public/images/BD-logo.png" alt="logo" />
+          </h1>
+        </>
+
+        <>
+          <button>로그아웃</button>
+        </>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
