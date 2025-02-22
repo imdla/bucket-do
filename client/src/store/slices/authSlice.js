e3a8e80caa1b86fdeee2a6caace6ce6bbde44d3c
imdla <imdla918@gmail.com>
2025-02-22 11:26:38 +0900

    [BD-53] chore: authSlice 초기값 설정
    - authSlice 초기값 설정
      - `token` : localStorage에서 `token` 가져오기
      - `isLoggedIn` : localStorage에서 `token` 여부에 따라 boolean 처리
      - `user`
        - `name` : localStorage에서 `userName` 가져오기

--- client/src/store/slices/authSlice.js
+++ client/src/store/slices/authSlice.js
@@ -1,1 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+// 초기값 (localStorage에서 해당 정보 받아옴)
+const initialState = {
+  token: localStorage.getItem('token'),
+  isLoggedIn: !!localStorage.getItem('token'),
+  user: {
+    name: localStorage.getItem('userName'),
+  },
+};
