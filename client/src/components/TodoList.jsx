0eaa2159117f98a120ce2bad03e86154342e98ae
imdla <imdla918@gmail.com>
2025-02-24 15:37:10 +0900

    [BD-49] feat: todoList 구조 작성

--- client/src/components/TodoList.jsx
+++ client/src/components/TodoList.jsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import todoApi from '../api/todoApi';
+import Todo from './Todo';
 
-export default function TodoList() {
+export default function TodoList({ bucketId }) {
+  const [todos, setTodos] = useState([]);
+
+  function fetchTodos() {
+    try {
+      const response = todoApi.getTodos(bucketId);
+      setTodos(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  // const todoList = todos.map((todo) => {
+  //   const { id, content, is_completed } = todo;
+
+  //   return (
+  //     <li key={id}>
+  //       <Todo
+  //         bucketId={bucketId}
+  //         todoId={id}
+  //         todoContent={content}
+  //         todoCompleted={is_completed}
+  //         fetchTodo={fetchTodos}
+  //       />
+  //     </li>
+  //   );
+  // });
+
   return (
-    <div>TodoList</div>
-  )
+    <div>
+      {/* <ul>{todoList}</ul> */}
+
+      <ul>
+        <li>
+          <Todo></Todo>
+        </li>
+        <li>
+          <Todo></Todo>
+        </li>
+        <li>
+          <Todo></Todo>
+        </li>
+      </ul>
+
+      <button>+</button>
+    </div>
+  );
 }
