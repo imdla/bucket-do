6ad7666ba074542e7497c1f925a982c27903c1d2
imdla <imdla918@gmail.com>
2025-02-24 11:17:56 +0900

    [BD-44] feat: Todo 컴포넌트 구조 작성

--- client/src/components/Todo.jsx
+++ client/src/components/Todo.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
 export default function Todo() {
-  return <div>Todo</div>;
+  return (
+    <div>
+      <button>체크 박스</button>
+
+      <input
+        id="todo"
+        name="todo"
+        type="text"
+        placeholder="투두 리스트 내용을 입력해주세요"
+        required
+        value={content}
+        onChange={handleTodoInput}
+      />
+
+      <button>X</button>
+    </div>
+  );
 }
