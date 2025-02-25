055f3b584bb55f0d6ed44f3b41dffb872fcdd829
imdla <imdla918@gmail.com>
2025-02-25 10:08:53 +0900

    [BD-97] feat: 모달 컴포넌트 생성

---
+++ client/src/components/Modal.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default Modal = ({
+  isOpen,
+  onClose,
+  content,
+  confirmText,
+  cancleText,
+  onConfirm,
+}) => {
+  if (!isOpen) return null;
+  return (
+    <div>
+      <div>
+        <p>{content}</p>
+
+        <div>
+          <button onClick={onClose}>{cancleText || '취소'}</button>
+          {onConfirm && <button onClick={onConfirm}>{confirmText || '취소'}</button>}
+        </div>
+      </div>
+    </div>
+  );
+};
