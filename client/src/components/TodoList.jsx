17420d19f21a796118fcbd8e1525d52b047bb8da
imdla <imdla918@gmail.com>
2025-03-02 18:24:40 +0900

    refactor: 이미지 get 에러 문제 해결

--- client/src/components/TodoList.jsx
+++ client/src/components/TodoList.jsx
@@ -14,40 +14,6 @@ export default function TodoList({
   isFixedTodoSelectable,
 }) {
   const [todoList, setTodoList] = useState([]);
-  const [isDarkBackground, setIsDarkBackground] = useState(false);
-
-  // 배경 이미지에 따른 폰트 컬러 지정
-  useEffect(() => {
-    const img = new Image();
-    img.crossOrigin = 'Anonymous';
-    img.src = imageUrl;
-
-    img.onload = () => {
-      const canvas = document.createElement('canvas');
-      const ctx = canvas.getContext('2d');
-
-      canvas.width = img.width;
-      canvas.height = img.height;
-      ctx.drawImage(img, 0, 0, img.width, img.height);
-
-      const imageData = ctx.getImageData(0, 0, img.width, img.height).data;
-      let totalBrightness = 0;
-      let pixelCount = 0;
-
-      for (let i = 0; i < imageData.length; i += 4) {
-        const r = imageData[i];
-        const g = imageData[i + 1];
-        const b = imageData[i + 2];
-
-        const brightness = (r * 299 + g * 587 + b * 114) / 1000;
-        totalBrightness += brightness;
-        pixelCount++;
-      }
-
-      const avgBrightness = totalBrightness / pixelCount;
-      setIsDarkBackground(avgBrightness < 128);
-    };
-  }, [imageUrl]);
 
   useEffect(() => {
     fetchTodos();
@@ -104,7 +70,6 @@ export default function TodoList({
               isFixed={isFixed}
               modalOpen={modalOpen}
               modalClose={modalClose}
-              isDarkBackground={isDarkBackground}
               isFixedTodoSelectable={isFixedTodoSelectable}
             />
           </li>
